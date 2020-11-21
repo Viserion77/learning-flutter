8def95962d5e76aa81e8b46733172c5479a69d25
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-11-20 22:07:19 -0300

    Feat: Add layout of screen to 'Criando transferência'

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,91 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(Bytebank());
+}
+
+class Bytebank extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        body: ListaTransferencias(),
-        appBar: AppBar(
-          title: Text('Transferências'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
+        body: FormularioTransferencia(),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Criando transferência'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24
+              ),
+              decoration: InputDecoration(
+                labelText: 'Número da conta',
+                hintText: '000',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24
+              ),
+              decoration: InputDecoration(
+                labelText: 'Valor',
+                hintText: '000',
+                icon: Icon(Icons.monetization_on),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: RaisedButton(
+              child: Text('Confirmar'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
 class ListaTransferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ItemTransferencia(Transferencia(1, 2)),
-        ItemTransferencia(Transferencia(3, 4)),
-        ItemTransferencia(Transferencia(5, 6)),
-      ],
+    return Scaffold(
+      body: Column(
+        children: [
+          ItemTransferencia(Transferencia(1, 2)),
+          ItemTransferencia(Transferencia(3, 4)),
+          ItemTransferencia(Transferencia(5, 6)),
+        ],
+      ),
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
 
 class ItemTransferencia extends StatelessWidget {
-
   final Transferencia _transferencia;
-  const ItemTransferencia(this._transferencia);
 
+  const ItemTransferencia(this._transferencia);
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +97,6 @@ class ItemTransferencia extends StatelessWidget {
       ),
     );
   }
-
 }
 
 class Transferencia {
