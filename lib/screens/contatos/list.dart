9c9f38c8aa65ef5649454ccda1d3a1bfc8db9f3d
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-11-21 12:32:01 -0300

    Feat: Create contact screens and model

---
+++ lib/screens/contatos/list.dart
@@ -0,0 +1,39 @@
+import 'package:bytebank/screens/contatos/form.dart';
+import 'package:flutter/material.dart';
+
+class ContactsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+      body: ListView(
+        children: [
+          Card(
+            child: ListTile(
+              title: Text(
+                'Alex',
+                style: TextStyle(fontSize: 24),
+              ),
+              subtitle: Text(
+                '1000',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => ContactForm(),
+            ),
+          ).then((newContact) => debugPrint(newContact.toString()));
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
